439bf1d1b3c206ec0392e16d1864f2f1e8924067
HurKyungsoo <herkyungsoo94@gmail.com>
2026-06-05 13:33:49 +0900

    Controller 구현

--- src/main/java/com/example/jpaexam/controller/PostController.java
+++ src/main/java/com/example/jpaexam/controller/PostController.java
@@ -7,10 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -24,5 +23,10 @@ public class PostController {
     public ResponseEntity<PostDto.Response> createPost(@Valid @RequestBody PostDto.CreateRequest request) {
         PostDto.Response response = postService.createPost(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostDto.Response>>getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
     }
 }
