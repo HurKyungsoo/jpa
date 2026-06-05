1cc4e0e2035677fa4c6c1e2768c5534d51dbad55
HurKyungsoo <herkyungsoo94@gmail.com>
2026-06-05 10:45:38 +0900

    table 이름 변경

--- src/main/java/com/example/jpaexam/entity/Post.java
+++ src/main/java/com/example/jpaexam/entity/Post.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "posts")
+@Table(name = "memberposts")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
